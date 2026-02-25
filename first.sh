bcaae52c011c7a6a61e212193b112ee1d60fd5a3
Hansel-vlnt <hanseldragon557@gmail.com>
2026-02-25 14:52:20 +0700

    Script automasi pertama saya

---
+++ first.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Mulai menjalankan automasi..."
+
+WAKTU=$(date +"%Y-%m-%d_%H-%M-%S")
+
+echo "Membuat folder baru dengan nama: backup_$WAKTU"
+
+mkdir "backup_$WAKTU"
+
+echo "Selesai! Folder berhasil dibuat."
